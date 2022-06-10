6d169d869b2a89f4a79f54848e4ebb9a594ba2a7
JorgeKz <jorgekzbra@gmail.com>
2022-06-10 20:27:22 -0300

    Primeira parte

---
+++ conteudo/02-front-end/bloco-10-introducao-a-react/dia-01-introducao-a-react/exercicio-dia-01/src/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    {'Jorge Casé'}
+                </h1>
+                <p>
+                    {'Encarando os desafios da Trybe e conciliando com a rotina de casa'}
+                </p>
+            </div>
+        );
+    }
+}
+
+export default About;
